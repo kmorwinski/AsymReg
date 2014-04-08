cc29f72ce389af0e0dcba4f5bff8f8ec2a85d0b1
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-04-08 11:21:48 +0200

    Member Variablen in mainwindow.h nach Funktion sortiert.

--- mainwindow.h
+++ mainwindow.h
@@ -26,35 +26,44 @@ protected:
     virtual void closeEvent(QCloseEvent *event);
 
 private slots:
+    // private slots for data source handling:
     void dataSourceChanged(QTableWidgetItem *item);
-    void plotPressureFunction(QAction *action = nullptr);
-    void runAsymReg();
     void saveDataSource();
     void selectDataSource(QAction *action);
+
+    void plotPressureFunction(QAction *action = nullptr);
+    void runAsymReg();
+
+    // private slots for viewing plots:
     void showSvgViewer(const QString &path);
 
 private:
+    // private functions for data source handling:
     QAction *addDataSourceAction(const QString &fileName);
+    int askToSaveDataSource(const QString &fileName);
     void discardDataSource();
     void loadDataSourceToTableWidget();
-    int askToSaveDataSource(const QString &fileName);
     void saveDataSource(const QString &fileName, bool reload = true);
 
+    // read and save Plotter-Settings from/to JSON files:
     bool loadPlotterSettings(const QString &fileName, PlotterSettings *sett) const;
     bool savePlotterSettings(const QString &fileName, const PlotterSettings *sett) const;
 
     PlotterSettings *m_pressureFunctionPlotSettings;
     QAction *m_confNplotPressFuncAction;
     QAction *m_plotPressFuncAction;
+
+    // members for data source handling:
     QActionGroup *m_dataSourceSelectGroup;
-    QDateTime m_plotTime;
-    QFileSystemWatcher *m_plotWatcher;
-    QList<QPointer<SvgViewer> > m_svgViewerList;
     QPushButton *m_dataSourceSelectButton;
     QPushButton *m_dataSourceSaveButton;
     QTableWidget *m_dataSourceTableWidget;
-
     bool m_dataSourceChanged;
+
+    // members for viewing plots:
+    QList<QPointer<SvgViewer> > m_svgViewerList;
+    QDateTime m_plotTime;
+    QFileSystemWatcher *m_plotWatcher;
 };
 
 #endif // MAINWINDOW_H_
