d233e7dd788f030bb8823183b240a4d6f61658d4
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-04-12 17:35:18 +0200

    Einfache Kommandozeilen Programm hinzugefügt.

---
+++ main-cl.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "****" << std::endl;
+    std::cout << "** Asymptotical Regularization in Computer Tomographie" << std::endl;
+    std::cout << "** Example: Schlieren Imaging" << std::endl;
+    std::cout << "****" << std::endl;
+
+    return 0;
+}
