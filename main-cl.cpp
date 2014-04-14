68901a12f892c18a03b1d96459f66c95eba0a8be
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-04-14 11:07:38 +0200

    Einlesen einer Datei für die Source-Daten und Zuweisung dieser Daten an AsymReg Klasse.
    Programm ist festgelegt auf die Datei "../data/data-11x11.csv".

--- main-cl.cpp
+++ main-cl.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
+#include "asymreg.h"
+
+#define DATA_FILE  "../data/data-11x11.csv"
+
+using namespace Eigen;
 using ts = std::string; // ts (ToString) is much shorter
 
 // private functions:
@@ -18,6 +24,26 @@ int main(int argc, char **argv)
     print_line("Asymptotical Regularization in Computer Tomographie");
     print_line("Example: Schlieren Imaging");
     print_end();
+
+    print_begin();
+    print_line_begin(ts("Loading data from file: \"") + DATA_FILE);
+
+    MatrixXd zMat(ASYMREG_DATSRC_SIZE, ASYMREG_DATSRC_SIZE);
+    std::fstream fs;
+    fs.open(DATA_FILE, std::fstream::in);
+    if (fs.is_open()) {
+        fs >> zMat;
+        print_line_end("\" ok!");
+    } else {
+        zMat.setZero();
+        print_line_end("\" failed!");
+    }
+
+    print_line("source data: ");
+    std::cout << zMat << std::endl;
+    print_end();
+
+    AsymReg::createSourceFunction(zMat);
 
     return 0;
 }
