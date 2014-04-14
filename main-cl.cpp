75eaef3d5abdf175d855b000c127fe1f16742409
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-04-14 11:06:49 +0200

    Funktion für die Text-Ausgabe hinzugefügt.

--- main-cl.cpp
+++ main-cl.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv)
+using ts = std::string; // ts (ToString) is much shorter
+
+// private functions:
+static void print(const std::string &text);
+static void print_begin();
+static void print_end();
+static void print_line(const std::string &text);
+static void print_line_begin(const std::string &text);
+static void print_line_end(const std::string &text);
+
+// function implementations:
+int main(int argc, char **argv)
 {
-    std::cout << "****" << std::endl;
-    std::cout << "** Asymptotical Regularization in Computer Tomographie" << std::endl;
-    std::cout << "** Example: Schlieren Imaging" << std::endl;
-    std::cout << "****" << std::endl;
+    print_begin();
+    print_line("Asymptotical Regularization in Computer Tomographie");
+    print_line("Example: Schlieren Imaging");
+    print_end();
 
     return 0;
+}
+
+void print_begin()
+{
+    std::cout << "****" << std::endl;
+}
+
+void print_end()
+{
+    std::cout << "****" << std::endl << std::endl;
+}
+
+void print_line(const std::string &text)
+{
+    print_line_begin(text);
+    std::cout << std::endl;
+}
+
+void print_line_begin(const std::string &text)
+{
+    std::cout << "** " << text;
+}
+
+void print_line_end(const std::string &text)
+{
+    std::cout << " " << text << std::endl;
 }
