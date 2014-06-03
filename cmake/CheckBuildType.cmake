e4392baa117b6be157d2d62d9df878a639e95aad
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-06-03 16:37:22 +0200

    CMake: Es werden nur bekannte CMAKE_BUILD_TYPEs zugelassen.
    Beim Aufruf von cmake kann man sich schnell mal vertun, wenn es
    um den CMAKE_BUILD_TYPE geht und steht am Ende ohne Debug-Symbole dar.
    
    Änderungen:
     - Neues CMake-Modul CheckBuildType.cmake. Dieses Modul bricht die
    Konfiguration ab, wenn CMAKE_BUILD_TYPE unbekannt ist.
     - CMake setzt "-DNDEBUG" selber, d.h. wir brauchen uns nur noch
    um "-DQT_NO_DEBUG" zu kümmern.
     - Setze "-DQT_NO_DEBUG" bei allen CMAKE_BUILD_TYPE außer bei Debug. So
    macht es CMake auch mit "-DNDEBUG"

---
+++ cmake/CheckBuildType.cmake
@@ -0,0 +1,20 @@
+macro(check_build_type)
+    #string(TOUPPER ${CMAKE_BUILD_TYPE} type)
+    #message(STATUS ${CMAKE_BUILD_TYPE})
+
+    # user has not defined CMAKE_BUILD_TYPE:
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+        message(FATAL_ERROR "You have to set CMAKE_BUILD_TYPE. If you have no clue, then use RelWithDebInfo: \n\"cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..\"")
+    endif()
+
+    # user has defined CMAKE_BUILD_TYPE. Let's see if we know and support it
+    string(TOUPPER ${CMAKE_BUILD_TYPE} type)
+    if((NOT "${type}" STREQUAL "DEBUG")          AND
+       (NOT "${type}" STREQUAL "RELEASE")        AND
+       (NOT "${type}" STREQUAL "RELWITHDEBINFO") AND
+       (NOT "${type}" STREQUAL "MINSIZEREL"))
+        # ... no we dont!!!
+        message(FATAL_ERROR "Your selected CMAKE_BUILD_TYPE: \"${CMAKE_BUILD_TYPE}\" is wrong or unkwon. Please use \"Debug\", \"Relase\", \"RelWithDebInfo\" or \"MinSizeRel\".")
+    endif()
+
+endmacro(check_build_type)
