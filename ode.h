fc71c1708f22d8c6ed2c90821b61f660ae4d36ca
Kai Morwinski <kai.morwinski@stud.uni-due.de>
2014-06-23 20:03:47 +0200

    Runge-Kutta DGL-Löser 4. Ordnung

--- ode.h
+++ ode.h
@@ -38,6 +38,28 @@ void rk2(const int angles, const EigenBase<Derived> &X, const Derived *dXdt,
     }
 }
 
+// Runge-Kutta 4th order
+template <typename Derived, typename DerivsFunc>
+void rk4(const int angles, const EigenBase<Derived> &X, const Derived *dXdt,
+           const typename Derived::Scalar h,
+           EigenBase<Derived> &Xout, DerivsFunc &derivs)
+{
+    typedef typename Derived::Scalar Scalar;
+
+    Xout.derived() = X;
+
+    Derived K1, K2, K3, K4;
+
+    for (int i = 0; i < angles; ++i) {
+        K1 = .5 * h * dXdt[i];
+        derivs(i,          K1 + X.derived(), K2);
+        derivs(i, .5 * h * K2 + X.derived(), K3);
+        derivs(i,      h * K3 + X.derived(), K4);
+
+        Xout.derived() += (1./6.*(K1 + h*K4) + 1./3.*(K2 + K3)) / Scalar(angles);
+    }
+}
+
 } // namespace ODE
 
 #endif // ODE_H_
